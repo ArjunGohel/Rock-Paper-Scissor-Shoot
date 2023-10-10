b28f525baea786e3c71fb3ed936aead8693558d9
Arjun Gohel <arjunjg27@gmail.com>
2023-10-10 18:58:08 -0400

    Update game.py

--- game.py
+++ game.py
@@ -11,28 +11,28 @@ user_option = user
 # countdown
 print("-")
 print("Rock")
-tm.sleep(2)
+tm.sleep(0.5)
 print("paper")
-tm.sleep(2)
+tm.sleep(0.5)
 print("scissor")
-tm.sleep(2)
+tm.sleep(0.5)
 print("Shoot!")
 
-tm.sleep(3)
+tm.sleep(1)
 
 #computer choice 
 choices = ["Rock","Paper", "Scissor"]
 r = ran.choice(choices)
-print("Computer:",r)
-print("User:", user)
+print("Computer:",r.lower())
+print("User:", user.lower())
 
-if user.lower == r.lower:
+if user.lower() == r.lower():
         print("TIE!")
     
 elif(
-    (user.lower == "rock" and r.lower == "scissor") or
-    (user.lower == "paper" and r.lower == "rock") or
-    (user.lower == "scissor" and r.lower == "paper")
+    (user.lower() == "rock" and r.lower() == "scissor") or
+    (user.lower() == "paper" and r.lower() == "rock") or
+    (user.lower() == "scissor" and r.lower() == "paper")
     ):
         print("You win")
 else:
